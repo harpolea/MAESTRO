25cc482d4d03da4ce419b98d9c410fc60dcfc1e9
harpolea <ah1e14@soton.ac.uk>
2016-06-14 17:57:15 +0100

    Functions for conservative to primative conversion

--- Source/metric.f90
+++ Source/metric.f90
@@ -15,9 +15,10 @@ module metric_module
 
     implicit none
 
-    private
+    private :: root_find_on_me, Newton_Raphson
 
-    public :: make_weak_field, inverse_metric, calcW, calcu0, g, christoffels
+    public :: make_weak_field, inverse_metric, calcW, calcu0, &
+              g, christoffels, cons_to_prim
 
 contains
 
@@ -276,10 +277,150 @@ contains
             enddo
         enddo
 
-
-
-
-
     end subroutine christoffels
+
+    subroutine cons_to_prim(s, u, alpha, s_prim, u_prim, mla)
+        use variables, only: rho_comp, rhoh_comp, spec_comp
+        ! FIXME: need to import thermal gamma
+        use probin_module, only : gamm_therm
+
+        type(multifab), intent(in) :: s(:)
+        type(multifab), intent(in) :: u(:)
+        type(multifab), intent(in) :: alpha(:)
+        type(multifab), intent(in) :: s_prim(:)
+        type(multifab), intent(in) :: u_prim(:)
+        type(ml_layout)   , intent(inout) :: mla
+
+        real(kind=dp_t), pointer ::  sp(:,:,:,:)
+        real(kind=dp_t), pointer ::  up(:,:,:,:)
+        real(kind=dp_t), pointer ::  alphap(:,:,:,:)
+        real(kind=dp_t), pointer ::  s_primp(:,:,:,:)
+        real(kind=dp_t), pointer ::  u_primp(:,:,:,:)
+
+        real(kind=dp_t) :: pmin, pmax, pbar, u0
+
+        integer :: i,j,k,m,n,nlevs,lo(mla%dim),hi(mla%dim)
+        logical :: fail
+
+        nlevs = mla%nlevel
+
+        do n=1,nlevs
+           do m=1,nfabs(s(n))
+               sp => dataptr(s(n), m)
+               up => dataptr(u(n), m)
+               alphap => dataptr(alpha(n), m)
+               s_primp => dataptr(s_prim(n), m)
+               u_primp => dataptr(u_prim(n), m)
+
+               do k = lo(3), hi(3)
+                   do j = lo(2), hi(2)
+                       do i = lo(1), hi(1)
+                           pmin = 0.d0
+                           pmax = 0.d0
+                           pbar = 0.d0
+
+                           call Newton_Raphson(pmin, pmax, pbar, root_find_on_me, &
+                           sp(i,j,k,rho_comp), up(i,j,k,:), sp(i,j,k,rhoh_comp), alphap(i,j,k,1), fail)
+
+                           u0 = (sp(i,j,k,rhoh_comp) - sp(i,j,k,rho_comp)) * (gamm_therm - 1.d0) / (gamm_therm * pbar)
+
+                           s_primp(i,j,k,rho_comp) = sp(i,j,k,rho_comp) / u0
+                           s_primp(i,j,k,rhoh_comp) = sp(i,j,k,rhoh_comp) / u0
+                           u_primp(i,j,k,:) = up(i,j,k,:) * u0
+                       enddo
+                   enddo
+               enddo
+           enddo
+       enddo
+
+
+    end subroutine cons_to_prim
+
+    subroutine root_find_on_me(pnew, pbar, dp, D, U, Dh, alpha)
+        ! FIXME: need to import thermal gamma
+        use probin_module, only : gamm_therm
+
+        real(kind=dp_t), intent(out) :: pnew
+        real(kind=dp_t), intent(in) :: pbar
+        real(kind=dp_t), intent(inout) :: dp
+        real(kind=dp_t), intent(in) :: D
+        real(kind=dp_t), intent(in) :: U(3)
+        real(kind=dp_t), intent(in) :: Dh
+        real(kind=dp_t), intent(in) :: alpha
+
+        integer :: i
+        real(kind=dp_t) :: usq
+
+        usq = 0.d0
+
+        do i = 1,3
+            usq = usq + U(i)*U(i)
+        enddo
+
+        pnew = (gamm_therm - 1.d0) * (Dh/D - 1.d0) * D * sqrt(alpha**2 - usq) / gamm_therm - pbar
+
+        dp = usq * gamm_therm * pnew / Dh - 1.d0
+
+
+    end subroutine root_find_on_me
+
+    subroutine Newton_Raphson(xl,xh,xm,NonLinearEquation, D, U, Dh, alpha,fail)
+        ! Root finder taken from multimodel code
+      real(kind=dp_t), intent(inout) :: xl     ! root bracket 1
+      real(kind=dp_t), intent(inout) :: xh     ! root bracket 2
+      real(kind=dp_t), intent(out)   :: xm     ! root
+      external NonLinearEquation        ! subroutine for non-linear equation
+      real(kind=dp_t), intent(in) :: D
+      real(kind=dp_t), intent(in) :: U(3)
+      real(kind=dp_t), intent(in) :: Dh
+      real(kind=dp_t), intent(in) :: alpha
+      logical, intent(out) :: fail
+
+      integer  :: k                      ! iteration count
+      real(kind=dp_t) :: xn, f, df, error
+      real(kind=dp_t), parameter :: zero=0.0d0, half=0.5d0
+      integer, parameter :: ITERMAX=3000 ! max number of iteration
+      real(kind=dp_t), parameter :: TOLERANCE=1.d-14 ! tolerance for error
+
+      call NonLinearEquation(xm,xl,df,D, U, Dh, alpha)  ! Set xm=f(xl)
+      call NonLinearEquation(xn,xh,df,D, U, Dh, alpha)  ! Set xn=f(xh)
+      if (xm*xn.gt.zero) then           ! check if function changes sign
+         xl = 0.0d0
+         xh = 10.0d0**(14)
+      endif
+      if (xm > zero) then               ! Rearrange so that f(xl)< 0.d0 < f(xh)
+         xm = xl
+         xl = xh
+         xh = xm
+      endif
+
+      error = abs(xh-xl)                ! error is width of bracketing interval
+      xm = half*(xl+xh)                 ! Initialize guess for root
+      k = 0                             ! initialize iteration count
+      do while (error > TOLERANCE .and. k < ITERMAX) ! iterate
+         k = k+1                         ! increment iteration count
+         call NonLinearEquation(f,xm,df,D, U, Dh, alpha) ! calculate f(xm), df(xm)
+         if (f > zero) then              ! Update root bracketing
+            xh = xm                       ! update high
+         else
+            xl = xm                       ! update low
+         endif
+         xn = xm - f/df                 ! Tentative newton-Raphson step
+         if ( (xn-xl)*(xn-xh) > zero ) then ! check if new root falls within bracket
+            xm = half* (xh+xl)            ! if no use a Bisection step
+            error = abs(xh-xl)            ! error is width of interval
+         else
+            error = abs(xn-xm)            ! if within bracket: error is change in root
+            xm = xn                       ! update successful Newton-Raphson step
+         endif
+      enddo
+      fail = .true.
+      if (error > TOLERANCE) then       ! Check if solution converged
+         write(*,*) 'solution did not converge, xn, NonLinearEquation(xn),D(xn)'
+         write(*,*) xn, f, error
+         fail = .true.
+      endif
+
+    end subroutine Newton_Raphson
 
 end module metric_module
