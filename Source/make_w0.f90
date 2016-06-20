d2c5dafe2374bde8a48d6b38ad4c6e68ac2a4986
harpolea <ah1e14@soton.ac.uk>
2016-06-20 16:06:37 +0100

    Got to start of main iteration loop

--- Source/make_w0.f90
+++ Source/make_w0.f90
@@ -428,6 +428,7 @@ contains
     u   = ZERO
 
     !$OMP PARALLEL DO PRIVATE(r,dpdr)
+    print * , 'base cutoff coord', base_cutoff_density_coord(nlevs_radial)
     do r=1,base_cutoff_density_coord(nlevs_radial)
        A(r) = gamma1bar_nph_fine(r-1) * p0_nph_fine(r-1)
        A(r) = A(r) / dr(nlevs_radial)**2
